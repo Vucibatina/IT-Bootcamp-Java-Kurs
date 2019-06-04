8bdc013f4e2d90be6d6a0ce14463d93769e12ee1
Radovic <vradovic2@bloomberg.com>
2019-06-04 14:45:18 -0400

    Test zavrsetak

--- Testovi/Test1.java
+++ Testovi/Test1.java
@@ -1,3 +1,21 @@
+public class Dog 
+{ 
+    
+    // Instance Variables 
+    String name; 
+    
+    // Brojaci
+    public int brojac1=0;
+    public static int brojac2=0;
+
+    // Constructor Declaration of Class 
+    public Dog(String name, ) 
+    { 
+        this.name = name;
+        brojac1+=1;
+        brojac2+=1; 
+    } 
+}
 
 public class TestingVuk {
 
@@ -35,7 +53,48 @@ public class TestingVuk {
 			if (i%3==0) continue;
 			System.out.println(i);
 		}
-				
+
+                // Objekti - sta ocekujemo kao output
+		Dog vucjak=new Dog("reks");
+		Dog buldog=new Dog("dzeki");
+		Dog dzukac=new Dog("baki");
+		System.out.println(vucjak.brojac1);
+		System.out.println(vucjak.brojac2);
+			
+		// Matrice - sta ocekujemo kao output
+		int mat[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } }; 
+		System.out.println(mat[0][2]);
+
 	}
 
 }
+
+
+/*
+SQL 
+
+Tabela: radnici
+Ime	Plata	MaticniBroj
+Ana	1000	12345
+Marko	800	23242
+Jovana	2000	2343423
+Goran	1200	23455
+
+Tabela: Izostanci
+MaticniBroj	Datum
+23242		2/2/2019
+23242		2/3/2019
+2343423		2/2/2019
+
+Problem 1:
+Izlistaj imena radnika sa platom manjom od 1000
+
+Problem 2:
+Izlistaj imena ljudi i datum izostanka
+
+Problem 3:
+Izlistaj imena ljudi i broj izostanaka
+
+
+
+*/
